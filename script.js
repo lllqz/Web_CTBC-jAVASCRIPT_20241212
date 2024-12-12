c754f6ba33ad7a7437c04abea498d72f05537e88
lllqz <candy0916.lin@gmail.com>
2024-12-12 15:01:56 +0800

    Update 建立基本架構

--- script.js
+++ script.js
@@ -29,3 +29,99 @@ console.log(fontColor);
 //設定變數
 test = 9;
 console.log(test);
+
+// 資料類型
+let varNumber = 1.5;     //數值
+let varString = "字串";  //字串
+let varBool = true;      //布林值(true、false)
+
+console.log(varNumber);
+console.log(varString);
+console.log(varBool);
+
+// typeof 判斷資料類型
+console.log(typeof varNumber);
+console.log(typeof varString);
+console.log(typeof varBool);
+
+// 運算子
+// 加減乘除餘次方
+console.log(10+3);
+console.log(10-3);
+console.log(10*3);
+console.log(10/3);
+console.log(10%3);    //10除以3=3餘1
+console.log(10**3);   //10的3次方
+
+// 指定運算子 (指定符號=)
+//1+2=3 數學內的等號，左右兩邊一定相等，由左至右執行
+//=指定，左右兩邊不一定相等，由右至左執行
+let numberTest = 99+1;
+console.log(numberTest);
+numberTest++;
+console.log(numberTest);
+numberTest--;
+console.log(numberTest);
+numberTest+=10;           //適用於+-*/%
+console.log(numberTest);
+numberTest-=10;
+console.log(numberTest);
+
+console.log("%c比較運算子", "color: pink");
+// > < >= <= === !== (執行結果是布林值)
+console.log(10 > 5);
+console.log(10 < 5);
+console.log(10 >= 5);
+console.log(10 <= 5);
+console.log(10 === 5); //等於 F
+console.log(10 !== 5); //不等於 T
+
+// == 與 === (嚴格比較，數值與類型都要相同)
+let varNumber10 =10;
+let varString10 = "10";
+
+console.log(varNumber10 == varString10);
+console.log(varNumber10 === varString10);  //嚴格比較，資料類型不同，結果為 faise
+
+console.log("%c判斷式", "color: pink");
+//判斷式語法
+// if(布林值) {布林值等於 true 執行此區塊 block }
+if (true) {                         //true會有執行結果，false 程式會呈現灰階不會執行
+    console.log("這裡是判斷式");
+}
+
+var score = 99;
+
+// if 要在最上方
+// else if 無限次數
+// else 要在最下方
+if (score >= 60) {
+    console.log("你及格了");
+}
+else if (score >= 50) {
+    console.log("去補考");
+}
+else {
+    console.log("你被當了");
+}
+
+// 練習
+var hp = 59;
+if (hp <= 0) {
+    console.log("你死亡了");
+}
+// &&並且
+else if (hp <=60) {
+    console.log("警告");
+}
+else {
+    console.log("安全");
+}
+
+console.log("%c迴圈", "color: pink");
+// for 迴圈
+// for 語法
+// for (初始值; 條件; 迭代器) {程式區塊}
+for (let index = 0; index < 10; index++) {
+    console.log("迴圈", index);
+}
